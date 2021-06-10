c3cb92ec93ca0bc4d59d6275ac16607406ef1838
--local <alexmenezesferreira@gmail.com>
2021-06-10 20:34:44 -0300

    10062021

--- admin/script.js
+++ admin/script.js
@@ -26,9 +26,9 @@ function menuconstrucao(){
 
     emconstrucao.innerHTML += '<img src="https://vignette.wikia.nocookie.net/tsrd/images/6/69/Construcao1-e1431697963872.gif/revision/latest?cb=20171123013110&path-prefix=pt-br" alt="Página em Construção">'
 
-    emconstrucao.innerHTML += '<div><iframe width="200" height="200" src="https://www.youtube.com/embed/c-3vPxKdj6o?list=PLY5G7sHQKEgtcPTtVYyB2qORC3HVLJ7VS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+    // emconstrucao.innerHTML += '<div><iframe width="200" height="200" src="https://www.youtube.com/embed/c-3vPxKdj6o?list=PLY5G7sHQKEgtcPTtVYyB2qORC3HVLJ7VS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
     
-    emconstrucao.innerHTML += '<style>div{margin: auto;border-radius: 25px; width:500px; heigth:500px;}</style>'
+    // emconstrucao.innerHTML += '<style>div{margin: auto;border-radius: 25px; width:500px; heigth:500px;}</style>'
     
     cria.innerHTML += ''
     
